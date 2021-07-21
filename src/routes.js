5b71beffbf82c8a196d65721970a88b7b318c992
isma <ismail.abbes@adriver.co>
2021-07-21 14:54:02 +0200

    alert

--- src/routes.js
+++ src/routes.js
@@ -89,4 +89,25 @@ module.exports = function(app){
         process.exit(1);
       });
     });
+
+    app.get('/getAlertByTimeAndTopic/:time/:sensor', (req, res) => {
+        (async () => {
+          const query = 'from(bucket: "mqtt_consumer")|> range(start: -' + req.params.time + 'm)|> filter(fn: (r) => r["_field"] == "alert" or r["_field"] == "data_value" ) |> filter(fn: (r) => r["_measurement"] == "'+ req.params.sensor +'")'
+          const influxdb = new Influxdb({
+            host: process.env.INFLUX_HOST,
+            token: process.env.INFLUX_TOKEN,
+            port: 443,
+          });
+        
+          const result = await influxdb.query(
+            { org: process.env.INFLUX_ORG },
+            { query: query }
+          );
+         res.status(200).json(result)
+        
+        })().catch(error => {
+          console.error('\n🐞 An error occurred!', error);
+          process.exit(1);
+        });
+      });
 }
