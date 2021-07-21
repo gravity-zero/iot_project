2146657ec0fb19d15b2809e341c7fb0715fb7de3
isma <ismail.abbes@adriver.co>
2021-07-21 20:12:53 +0200

    new routes

--- src/routes.js
+++ src/routes.js
@@ -13,67 +13,67 @@ module.exports = function(app){
     });
     
     app.get('/getFieldByTime/:time', (req, res) => {
-      (async () => {
+        (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: -' + req.params.time + 'm)|> filter(fn: (r) => r["_field"] == "data_value")'
-      
+        
         const result = await influxdb.query(
-          { org: process.env.INFLUX_ORG },
-          { query: query }
+            { org: process.env.INFLUX_ORG },
+            { query: query }
         );
-       res.status(200).json(result)
-      
-      })().catch(error => {
+        res.status(200).json(result)
+        
+        })().catch(error => {
         console.error('\n🐞 An error occurred!', error);
         process.exit(1);
-      });
+        });
     });
     
     app.get('/getFieldByRange/:date1/:date2', (req, res) => {
-      (async () => {
+        (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: ' + req.params.date1 + ', stop:'+ req.params.date2 +')|> filter(fn: (r) => r["_field"] == "data_value")'
 
         const result = await influxdb.query(
-          { org: process.env.INFLUX_ORG },
-          { query: query }
+            { org: process.env.INFLUX_ORG },
+            { query: query }
         );
-       res.status(200).json(result)
-      
-      })().catch(error => {
+        res.status(200).json(result)
+        
+        })().catch(error => {
         console.error('\n🐞 An error occurred!', error);
         process.exit(1);
-      });
+        });
     });
     
     app.get('/getFieldByTimeAndTopic/:time/:sensor', (req, res) => {
-      (async () => {
+        (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: -' + req.params.time + 'm)|> filter(fn: (r) => r["_field"] == "data_value") |> filter(fn: (r) => r["_measurement"] == "'+ req.params.sensor +'")'
-      
+        
         const result = await influxdb.query(
-          { org: process.env.INFLUX_ORG },
-          { query: query }
+            { org: process.env.INFLUX_ORG },
+            { query: query }
         );
-       res.status(200).json(result)
-      
-      })().catch(error => {
+        res.status(200).json(result)
+        
+        })().catch(error => {
         console.error('\n🐞 An error occurred!', error);
         process.exit(1);
-      });
+        });
     });
     
     app.get('/getFieldByRangeAndTopic/:date1/:date2/:sensor', (req, res) => {
-      (async () => {
+        (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: ' + req.params.date1 + ', stop:'+ req.params.date2 +')|> filter(fn: (r) => r["_field"] == "data_value") |> filter(fn: (r) => r["_measurement"] == "'+ req.params.sensor +'")'
 
         const result = await influxdb.query(
-          { org: process.env.INFLUX_ORG },
-          { query: query }
+            { org: process.env.INFLUX_ORG },
+            { query: query }
         );
-       res.status(200).json(result)
-      
-      })().catch(error => {
+        res.status(200).json(result)
+        
+        })().catch(error => {
         console.error('\n🐞 An error occurred!', error);
         process.exit(1);
-      });
+        });
     });
 
     app.get('/getAlertByTimeAndTopic/:time/:sensor', (req, res) => {
