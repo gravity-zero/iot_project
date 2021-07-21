c721239164812de7610f0152ba6a4d2a39427da2
isma <ismail.abbes@adriver.co>
2021-07-21 19:33:01 +0200

    opti

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,12 @@ const Influxdb = require('influxdb-v2');
 
 module.exports = function(app){
 
+    const influxdb = new Influxdb({
+        host: process.env.INFLUX_HOST,
+        token: process.env.INFLUX_TOKEN,
+        port: 443,
+      });
+
     app.get('/', (req, res) => {
       res.send('WEB SERVICE LAUNCH');
     });
@@ -9,11 +15,6 @@ module.exports = function(app){
     app.get('/getFieldByTime/:time', (req, res) => {
       (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: -' + req.params.time + 'm)|> filter(fn: (r) => r["_field"] == "data_value")'
-        const influxdb = new Influxdb({
-          host: process.env.INFLUX_HOST,
-          token: process.env.INFLUX_TOKEN,
-          port: 443,
-        });
       
         const result = await influxdb.query(
           { org: process.env.INFLUX_ORG },
@@ -30,12 +31,7 @@ module.exports = function(app){
     app.get('/getFieldByRange/:date1/:date2', (req, res) => {
       (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: ' + req.params.date1 + ', stop:'+ req.params.date2 +')|> filter(fn: (r) => r["_field"] == "data_value")'
-        const influxdb = new Influxdb({
-          host: process.env.INFLUX_HOST,
-          token: process.env.INFLUX_TOKEN,
-          port: 443,
-        });
-      
+
         const result = await influxdb.query(
           { org: process.env.INFLUX_ORG },
           { query: query }
@@ -51,11 +47,6 @@ module.exports = function(app){
     app.get('/getFieldByTimeAndTopic/:time/:sensor', (req, res) => {
       (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: -' + req.params.time + 'm)|> filter(fn: (r) => r["_field"] == "data_value") |> filter(fn: (r) => r["_measurement"] == "'+ req.params.sensor +'")'
-        const influxdb = new Influxdb({
-          host: process.env.INFLUX_HOST,
-          token: process.env.INFLUX_TOKEN,
-          port: 443,
-        });
       
         const result = await influxdb.query(
           { org: process.env.INFLUX_ORG },
@@ -72,12 +63,7 @@ module.exports = function(app){
     app.get('/getFieldByRangeAndTopic/:date1/:date2/:sensor', (req, res) => {
       (async () => {
         const query = 'from(bucket: "mqtt_consumer")|> range(start: ' + req.params.date1 + ', stop:'+ req.params.date2 +')|> filter(fn: (r) => r["_field"] == "data_value") |> filter(fn: (r) => r["_measurement"] == "'+ req.params.sensor +'")'
-        const influxdb = new Influxdb({
-          host: process.env.INFLUX_HOST,
-          token: process.env.INFLUX_TOKEN,
-          port: 443,
-        });
-      
+
         const result = await influxdb.query(
           { org: process.env.INFLUX_ORG },
           { query: query }
@@ -93,11 +79,6 @@ module.exports = function(app){
     app.get('/getAlertByTimeAndTopic/:time/:sensor', (req, res) => {
         (async () => {
           const query = 'from(bucket: "mqtt_consumer")|> range(start: -' + req.params.time + 'm)|> filter(fn: (r) => r["_field"] == "alert" or r["_field"] == "data_value" ) |> filter(fn: (r) => r["_measurement"] == "'+ req.params.sensor +'")'
-          const influxdb = new Influxdb({
-            host: process.env.INFLUX_HOST,
-            token: process.env.INFLUX_TOKEN,
-            port: 443,
-          });
         
           const result = await influxdb.query(
             { org: process.env.INFLUX_ORG },
